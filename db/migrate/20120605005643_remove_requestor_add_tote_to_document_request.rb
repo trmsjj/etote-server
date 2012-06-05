54a211b941663b06d902a248ad23efe093cd23d3
Ray Tiley <raytiley@gmail.com>
2012-06-04 20:34:35 -0500

    I am dumb

--- db/migrate/20120605005643_remove_requestor_add_tote_to_document_request.rb
+++ db/migrate/20120605005643_remove_requestor_add_tote_to_document_request.rb
@@ -1,8 +1,8 @@
 class RemoveRequestorAddToteToDocumentRequest < ActiveRecord::Migration
 	def change
 		change_table :document_requests do |t|
-		t.remove :requestor_id
-		t.belongs_to :tote
-	end
+			t.remove :requestor_id
+			t.belongs_to :tote
+		end
 	end
 end
